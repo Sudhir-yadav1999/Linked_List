5864975e581383337b19ad3c29becbdd138bcd41
Sudhir yadav <54906044+Sudhir-yadav1999@users.noreply.github.com>
2021-07-13 12:15:54 +0530

    Update Remove_duplicates_sorted.cpp

--- Remove_duplicates_sorted.cpp
+++ Remove_duplicates_sorted.cpp
@@ -31,3 +31,22 @@ void removeDuplicates(struct Node* head)
  
   
 }
+Time Complexity= o(n)
+    
+========    
+Hash Map 
+========
+
+void removeDuplicates(Node* head)
+{
+    unordered_map<int, bool> track;
+    Node* temp = head;
+    while (temp) {
+        if (track.find(temp->data) == track.end()) {
+            cout << temp->data << " ";
+        }
+        track[temp->data] = true;
+        temp = temp->next;
+    }
+}
+
