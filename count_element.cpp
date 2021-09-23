f231cace6fcabba691ff90eb89658c58748cb970
Sudhir yadav <54906044+Sudhir-yadav1999@users.noreply.github.com>
2021-09-23 12:22:53 +0530

    Update count_element.cpp

--- count_element.cpp
+++ count_element.cpp
@@ -29,3 +29,41 @@ while(head!=NULL)
   head=head->next;
 }
 return count;
+
+
+----------
+search
+----------
+~Recursive
+
+bool search(struct Node* head, int x) 
+{ 
+    // Base case 
+    if (head == NULL) 
+        return false; 
+      
+    // If key is present in current node, return true 
+    if (head->key == x) 
+        return true; 
+  
+    // Recur for remaining list 
+    return search(head->next, x); 
+} 
+
+~Iterative
+
+bool search(Node* head, int x) 
+{ 
+    Node* current = head; // Initialize current 
+    while (current != NULL) 
+    { 
+        if (current->key == x) 
+            return true; 
+        current = current->next; 
+    } 
+    return false; 
+} 
+
+
+
+
